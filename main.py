290e4e33679e8b0bc688c8e6e0909a9690602bbc
Nikolay.Nikolov2 <nikolay.nikolov2@adastragrp.com>
2022-02-10 14:28:39 +0200

    final

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-# import sys
-# print(sys.getrecursionlimit())
-# sys.setrecursionlimit(1000)
 import math
 import time
 import functools
@@ -9,63 +6,63 @@ import functools
 """
 function which returns the average of any number of given parameters
 """
-# def average(*args: float) -> float:
-#     return sum(args) / len(args)
 
-# x = average(20, 50, 66, 32)
-# print(x)
+
+def average(*args: float) -> float:
+    return sum(args) / len(args)
+
+
+x = average(20, 50, 66, 32)
+print(x)
 
 
 """
 Lucas numbers, timer and memoize decorators
 """
 
-# def memoize(f):
-#     memo = {}
-#
-#     def helper(x):
-#         if x not in memo:
-#             memo[x] = f(x)
-#         return memo[x]
-#
-#     return helper
+
+def memoize(f):
+    memo = {}
+
+    def helper(x):
+        if x not in memo:
+            memo[x] = f(x)
+        return memo[x]
+
+    return helper
 
 
-# def timer(func):
-#     @functools.wraps(func)
-#     def wrapper(*args, **kwargs):
-#         start_time = time.perf_counter()
-#         value = func(*args, **kwargs)
-#         end_time = time.perf_counter()
-#         elapsed_time = end_time - start_time
-#         print(f"{func.__name__!r} finished in {elapsed_time:.4f} seconds.")
-#         return value
-#
-#     return wrapper
+def timer(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        value = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        elapsed_time = end_time - start_time
+        print(f"{func.__name__!r} finished in {elapsed_time:.4f} seconds.")
+        return value
+
+    return wrapper
 
 
-# # use decorator "timer" to show elapsed time
-# # timer
+# use decorator "timer" to show elapsed time
+# timer
 
-# # use decorator memoize for much faster calculations
-# @memoize
-# def lucas(n):
-#     if n == 0:
-#         return 2
-#     elif n == 1:
-#         return 1
-#     else:
-#         return lucas(n - 1) + lucas(n - 2)
-#
-#
-# print(lucas(100))
+# use decorator memoize for much faster calculations
+@memoize
+def lucas(n):
+    if n == 0:
+        return 2
+    elif n == 1:
+        return 1
+    else:
+        return lucas(n - 1) + lucas(n - 2)
 
 
-"""
-prime factorization of a number
-"""
+print(lucas(100))
 
 
+# prime factorization of a number
 def primes(n):
     while n % 2 == 0:
         print(2),
